a4e4df3fa84aeaf2b244eb4c745192e5de552198
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-06 17:11:41 +0100

    masster

--- foudnations/rspec_exercise_5/lib/exercise.rb
+++ foudnations/rspec_exercise_5/lib/exercise.rb
@@ -12,10 +12,16 @@ def zip(*arrays)
 
 end
 
+def prizz_proc(array,prc1,prc2)
+
+    new_array = []
+
+        array.each do |ele|
+            if prc1.call(ele) && !prc2.call(ele) || prc2.call(ele) && !prc1.call(ele)
+              new_array << ele
+            end  
+        end
+    new_array
+end
 
 
-array_1 = ['a', 'b', 'c']
-array_2 = [1, 2, 3] 
-array_3 = ['w', 'x', 'y']
-
-p zip(array_1, array_2)
