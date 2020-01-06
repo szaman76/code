08e4504f579f09374d5348f9907f08a5355514b8
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-06 17:07:22 +0100

    futsal

--- foudnations/rspec_exercise_5/lib/exercise.rb
+++ foudnations/rspec_exercise_5/lib/exercise.rb
@@ -1,4 +1,21 @@
 def zip(*arrays)
 
+    new_array = Array.new(3){Array.new}
+
+    arrays.each_with_index do |array,array_inde|
+        array.each_with_index do |ele,index|
+            new_array[index][array_inde] = ele
+        end
+    end
+
+    new_array
 
 end
+
+
+
+array_1 = ['a', 'b', 'c']
+array_2 = [1, 2, 3] 
+array_3 = ['w', 'x', 'y']
+
+p zip(array_1, array_2)
