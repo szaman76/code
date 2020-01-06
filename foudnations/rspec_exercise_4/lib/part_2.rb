b06ebb4c795ecbe4ff0ec9370f14f88b284de398
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-06 16:16:55 +0100

    exe 4

--- foudnations/rspec_exercise_4/lib/part_2.rb
+++ foudnations/rspec_exercise_4/lib/part_2.rb
@@ -1,6 +1,31 @@
 def proper_factors(number)
+    new_arr = []
 
-    
+    (1...number).each do |num|
+        if number % num == 0 
+            new_arr << num
+        end
+    end
+    new_arr
+end
 
+def aliquot_sum(number)
+    return proper_factors(number).sum
+end
 
+def perfect_number?(number)
+    return aliquot_sum(number) == number
+end
+
+def ideal_numbers(n)
+    new_arr = []
+    i = 6
+
+    while new_arr.length < n
+        if perfect_number?(i)
+            new_arr << i
+        end
+    i += 1
+    end
+    new_arr
 end
