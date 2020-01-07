347959b511a52cdf35d5b48d1391946c72586b21
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-08 00:39:58 +0100

    Nocturne in F# minor Op. 48 No. 2

--- foudnations/meddling_medley/problems.rb
+++ foudnations/meddling_medley/problems.rb
@@ -193,8 +193,158 @@ end
 # p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
 
 
+# conjunct_select
+# Write a method conjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed into all of the given procs.
+
+# Examples
+
+
+def conjunct_select(arr,*prcs)
+
+    new_arr = []
+        arr.each do |el|
+            count = 0
+            prcs.each do |prc|
+                if prc.call(el)
+                    count += 1
+                end
+            end
+            if count == prcs.length
+                new_arr << el
+            end
+        end
+
+    new_arr
+end
+
+# is_positive = Proc.new { |n| n > 0 }
+# is_odd = Proc.new { |n| n.odd? }
+# less_than_ten = Proc.new { |n| n < 10 }
+
+# # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+# # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+# # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
 
 
 
 
+# convert_pig_latin
+# Write a method convert_pig_latin that accepts a sentence as an argument. The method should translate the sentence according to the following rules:
 
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+# if the word begins with a vowel, simply add 'yay' to the end of the word (example: 'eat'->'eatyay')
+# if the word begins with a non-vowel, move all letters that come before the word's first vowel to the end of the word and add 'ay' (example: 'trash'->'ashtray')
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+# Examples
+
+def convert_pig_latin(sent)
+
+    #vowels = "aeiou"
+    result = sent.split(" ")
+
+    result.map! do |word|
+        if word.length < 3
+            word
+        elsif "aeiouAEIOU".include?(word[0])
+            word = word.capitalize +  "yay"
+        else
+            idx = word.index(/[aeiouAEIOU]/)
+            if word == word.capitalize
+                word = word[idx..-1].capitalize + word[0...idx].downcase + "ay"
+            else
+            word = word[idx..-1] + word[0...idx] + "ay"
+            end
+        end
+    end
+    result.join(" ")
+
+end
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
+
+
+
+# reverberate
+# Write a method reverberate that accepts a sentence as an argument. The method should translate the sentence according to the following rules:
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+# if the word ends with a vowel, simply repeat the word twice (example: 'like'->'likelike')
+# if the word ends with a non-vowel, repeat all letters that come after the word's last vowel, including the last vowel itself (example: 'trash'->'trashash')
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+# Examples
+
+def reverberate(sent)
+
+    #vowels = "aeiou"
+    result = sent.split(" ")
+
+    result.map! do |word|
+        if word.length < 3
+            word
+        elsif "aeiouAEIOU".include?(word[-1])
+            word += word.downcase
+        else
+            idx = word.rindex(/[aeiouAEIOU]/)
+            word += word[idx..-1]
+        end
+    end
+    result.join(" ")
+
+end
+
+# p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
+# p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
+# p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
+# p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
+
+
+# disjunct_select
+# Write a method disjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed into at least one of the given procs.
+
+# Examples
+
+def disjunct_select(arr,*prcs)
+
+    new_arr = []
+
+        arr.each do |el|
+            prcs.each do |prc|
+                if prc.call(el)
+                    new_arr << el
+                    break
+                end
+            end
+        end
+    new_arr
+end
+
+
+# longer_four = Proc.new { |s| s.length > 4 }
+# contains_o = Proc.new { |s| s.include?('o') }
+# starts_a = Proc.new { |s| s[0] == 'a' }
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+# ) # ["apple", "teeming"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o
+# ) # ["dog", "apple", "teeming", "boot"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o,
+#     starts_a
+# ) # ["ace", "dog", "apple", "teeming", "boot"]
