dd507531d8068d1d5c655f7fcd28d8a0083cf50f
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-07 19:33:13 +0100

    Oliver Cheatham, Get Down Saturday Night

--- foudnations/Nauseating Numbers/problems.rb
+++ foudnations/Nauseating Numbers/problems.rb
@@ -289,9 +289,54 @@ def matrix_addition_reloaded(*matrix)
 end
 
 
+# squarocol?
+# Write a method squarocol? that accepts a 2-dimensional array as an argument. 
+# The method should return a boolean indicating whether or not any row or column is completely filled with 
+# the same element. 
+# You may assume that the 2-dimensional array has "square" dimensions, meaning it's height is the same as it's width.
 
+# Examples
 
+# p squarocol?([
+#     [:a, :x , :d],
+#     [:b, :x , :e],
+#     [:c, :x , :f],
+# ]) # true
 
+# p squarocol?([
+#     [:x, :y, :x],
+#     [:x, :z, :x],
+#     [:o, :o, :o],
+# ]) # true
 
+# p squarocol?([
+#     [:o, :x , :o],
+#     [:x, :o , :x],
+#     [:o, :x , :o],
+# ]) # false
 
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 7],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # true
+
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 0],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # false
+
+def squarocol?(array)
+    arr = array.transpose
+
+    arr.each do |row|
+        if row.all?(row[0])
+            return true
+        end
+    end
+    false
+end
 
