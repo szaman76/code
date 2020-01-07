c05379de0f6c45ba3e6b62b8adb2cbb5a2cb01ff
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-07 22:51:03 +0100

    Wicked Game

--- foudnations/Perilous Procs/problems.rb
+++ foudnations/Perilous Procs/problems.rb
@@ -299,6 +299,120 @@ def proctition(array, &block)
 end
 
 
+# selected_map!
+# Write a method selected_map! that accepts an array and two procs as arguments. 
+# The method should replace the elements that return true when passed into the first proc with their return values when 
+# they are passed into the second proc. This method should mutate the input array and return nil.
+
+# Examples
+# is_even = Proc.new { |n| n.even? }
+# is_positive = Proc.new { |n| n > 0 }
+# square = Proc.new { |n| n * n }
+# flip_sign = Proc.new { |n| -n }
+
+# arr_1 = [8, 5, 10, 4]
+# p selected_map!(arr_1, is_even, square)     # nil
+# p arr_1                                     # [64, 5, 100, 16]
+
+# arr_2 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_2, is_even, flip_sign)  # nil
+# p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+# arr_3 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_3, is_positive, square) # nil
+# p arr_3                                     # [-10, 16, 49, 36, -2, -9]
+
+def selected_map!(array, prc_1, prc_2)
+    array.map! do |el|
+        if prc_1.call(el)
+            el = prc_2.call(el)
+        else
+            el
+        end
+    end
+    nil
+end
+
+# chain_map
+# Write a method that accepts any value and an array of procs as an argument. 
+# The method should return the final result of feeding the value through all of the procs. 
+# For example, if the array contains three procs, then:
+
+# the value is given to the first proc
+# the result of the first proc is given to the second proc
+# the result of the second proc is given to the third proc
+# the result of third proc is the final result
+# Examples
+# add_5 = Proc.new { |n| n + 5 }
+# half = Proc.new { |n| n / 2.0 }
+# square = Proc.new { |n| n * n }
+
+# p chain_map(25, [add_5, half])          # 15.0
+# p chain_map(25, [half, add_5])          # 17.5
+# p chain_map(25, [add_5, half, square])  # 225
+# p chain_map(4, [square, half])          # 8
+# p chain_map(4, [half, square])          # 4
+
+def chain_map(n,array)
+    array.each do |prc|
+        n = prc.call(n)
+    end
+    n
+end
+
+# proc_suffix
+# Write a method proc_suffix that accepts a sentence and a hash as arguments. 
+# The hash contains procs as keys and suffix strings as values. 
+# The method should return a new sentence where each word of the original sentence is 
+# appended with a suffix if the original word returns true when given to the corresponding proc key. 
+# If an original word returns true for multiple procs, then the suffixes should be appended in the order that they appear in the input hash.
+
+# Examples
+# contains_a = Proc.new { |w| w.include?('a') }
+# three_letters = Proc.new { |w| w.length == 3 }
+# four_letters = Proc.new { |w| w.length == 4 }
+
+# p proc_suffix('dog cat',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "dogo catlyo"
+
+# p proc_suffix('dog cat',
+#     three_letters => 'o',
+#     contains_a => 'ly'
+# )   # "dogo catoly"
+
+# p proc_suffix('wrong glad cat',
+#     contains_a => 'ly',
+#     three_letters => 'o',
+#     four_letters => 'ing'
+# )   # "wrong gladlying catlyo"
+
+# p proc_suffix('food glad rant dog cat',
+#     four_letters => 'ing',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "fooding gladingly rantingly dogo catlyo"
+
+
+def proc_suffix(string, hash)
+    new_string = string.split(" ")
+
+    new_string.each do |word|
+        pholder = word.dup
+        hash.each do |key,value|
+            if key.call(pholder)
+                word << value
+            end 
+        end
+    end
+
+    new_string.join(" ")
+end
+
+
+
+
 
 
 
