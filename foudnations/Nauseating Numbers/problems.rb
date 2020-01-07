91b429bd131c344105da01de27baf60a727cf7e4
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-07 20:25:27 +0100

    change - miracles

--- foudnations/Nauseating Numbers/problems.rb
+++ foudnations/Nauseating Numbers/problems.rb
@@ -296,7 +296,6 @@ end
 # You may assume that the 2-dimensional array has "square" dimensions, meaning it's height is the same as it's width.
 
 # Examples
-
 # p squarocol?([
 #     [:a, :x , :d],
 #     [:b, :x , :e],
@@ -340,3 +339,135 @@ def squarocol?(array)
     false
 end
 
+
+
+# squaragonal?
+# Write a method squaragonal? that accepts 2-dimensional array as an argument. 
+# The method should return a boolean indicating whether or not the array contains all of the same element across either of its diagonals. 
+# You may assume that the 2-dimensional array has "square" dimensions, meaning it's height is the same as it's width.
+
+# Examples
+# p squaragonal?([
+#     [:x, :y, :o],
+#     [:x, :x, :x],
+#     [:o, :o, :x],
+# ]) # true
+
+# p squaragonal?([
+#     [:x, :y, :o],
+#     [:x, :o, :x],
+#     [:o, :o, :x],
+# ]) # true
+
+# p squaragonal?([
+#     [1, 2, 2, 7],
+#     [1, 1, 6, 7],
+#     [0, 5, 1, 7],
+#     [4, 2, 9, 1],
+# ]) # true
+
+# p squaragonal?([
+#     [1, 2, 2, 5],
+#     [1, 6, 5, 0],
+#     [0, 2, 2, 7],
+#     [5, 2, 9, 7],
+# ]) # false
+
+def squaragonal?(array)
+    len = array.length - 1
+    base = array[0][0]
+    second_base = array[0][len]
+
+    (0..len).each do |i|
+        if base != array[i][i] && second_base != array[i][-i - 1]
+            return false
+        end
+    end
+    true
+end
+
+# pascals_triangle
+# Pascal's triangle is a 2-dimensional array with the shape of a pyramid. The top of the pyramid is the number 1. 
+# To generate further levels of the pyramid, every element is the sum of the element above and to the left with the element above and to the right.
+#  Nonexisting elements are treated as 0 when calculating the sum. For example, here are the first 5 levels of Pascal's triangle:
+
+#       1
+#      1 1
+#     1 2 1
+#    1 3 3 1
+#   1 4 6 4 1
+# Write a method pascals_triangle that accepts a positive number, n, as an argument and returns a 2-dimensional array representing 
+# the first n levels of pascal's triangle.
+
+# Examples
+
+def pascals_triangle(number)
+
+    
+
+
+
+end
+
+
+# p pascals_triangle(5)
+# # [
+# #     [1],
+# #     [1, 1],
+# #     [1, 2, 1],
+# #     [1, 3, 3, 1],
+# #     [1, 4, 6, 4, 1]
+# # ]
+
+# p pascals_triangle(7)
+# # [
+# #     [1],
+# #     [1, 1],
+# #     [1, 2, 1],
+# #     [1, 3, 3, 1],
+# #     [1, 4, 6, 4, 1],
+# #     [1, 5, 10, 10, 5, 1],
+# #     [1, 6, 15, 20, 15, 6, 1]
+# # ]
+
+# mersenne_prime
+# A "Mersenne prime" is a prime number that is one less than a power of 2. 
+# This means that it is a prime number with the form 2^x - 1, where x is some exponent. For example:
+
+# 3 is a Mersenne prime because it is prime and 3 = 2^2 - 1
+# 7 is a Mersenne prime because it is prime and 7 = 2^3 - 1
+# 11 is not a Mersenne prime because although it is prime, it does not have the form 2^x - 1
+# The first three Mersenne primes are 3, 7, and 31. 
+# Write a method mersenne_prime that accepts a number, n, as an argument and returns the n-th Mersenne prime.
+
+# Examples
+# p mersenne_prime(1) # 3
+# p mersenne_prime(2) # 7
+# p mersenne_prime(3) # 31
+# p mersenne_prime(4) # 127
+# p mersenne_prime(6) # 131071
+
+def prime?(n)
+    return false if n < 2
+
+    (2...n).each do |i|
+        return false if n % i == 0
+    end
+    true
+end
+
+def mersenne_prime(n)
+
+    power = 2
+    count = 0
+
+    while count < n
+        power *= 2
+        if prime?(power - 1)
+            count += 1
+        end
+    end
+
+    power - 1
+
+end
