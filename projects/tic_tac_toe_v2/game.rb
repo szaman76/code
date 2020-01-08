5a0d34cb79f388c61c582d58285b9518eae79ab3
Michal Szymanski <m.szymanski1995@gmail.com>
2020-01-08 17:27:49 +0100

    tic toc

---
+++ projects/tic_tac_toe_v2/game.rb
@@ -0,0 +1,44 @@
+require_relative "board"
+require_relative "human_player"
+
+class Game
+    attr_reader :current_player
+
+
+    def initialize(player_1_mark, player_2_mark)
+        @player_1 = HumanPlayer.new(player_1_mark)
+        @player_2 = HumanPlayer.new(player_2_mark)
+        @board = Board.new
+        @current_player = @player_1
+    end
+
+    def switch_turn
+        if @current_player == @player_1
+            @current_player = @player_2
+            true
+        else
+            @current_player = @player_1
+            true
+        end
+    end
+
+    def game
+
+        while @board.empty_positions?
+            position = @current_player.get_position
+            while !@board.place_mark(position,@current_player.mark)
+                position = @current_player.get_position
+            end
+            if @board.win?(@current_player.mark)
+                return
+            else
+                self.switch_turn
+            end
+        end
+
+        puts "draw"
+        return true
+    end
+
+end
+
---
+++ projects/tic_tac_toe_v2/human_player.rb
@@ -0,0 +1,42 @@
+
+require_relative "board"
+class HumanPlayer
+
+    attr_reader :mark
+
+    def initialize(mark)
+        @mark = mark
+    end
+    
+    def get_position
+        puts "enter the position as two numbers [row, col]"
+        puts "You are playing with #{@mark}"
+        pos = gets.chomp
+
+        if ("a".."z").include?(pos[0]) || ("a".."z").include?(pos[2]) 
+            p "please put numbers"
+            self.get_position
+            #false
+        end
+        if pos.length != 3
+            p "wrong positions put two numbers with space in between"
+            self.get_position
+            #false
+        end
+        if !(0..2).include?(pos[0].to_i) || !(0..2).include?(pos[2].to_i)
+            p "please put numbers in between 0 and 2"
+            self.get_position
+            #false
+        end
+        return [pos[0].to_i,pos[2].to_i]
+        true
+        
+    end
+
+end
+
+
+# b = HumanPlayer.new("X")
+
+# p b.get_position
+# #p b.mark_value
