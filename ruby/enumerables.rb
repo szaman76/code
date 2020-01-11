bb5d40586a5982c3700b5c48e8870980f2e3056a
michal <m.szymanski1995@gmail.com>
2020-01-11 21:30:21 +0100

    Rod Stewart - Young Turks

--- ruby/enumerables.rb
+++ ruby/enumerables.rb
@@ -39,7 +39,6 @@ require "byebug"
         end 
 
         def my_all?(&block)
-            debugger
             self.length.times do |i|
                 if !block.call(self[i])
                     return false
@@ -48,10 +47,43 @@ require "byebug"
             true
         end 
 
+        def my_flatten
+            new_arr = []
+                self.each do |el|
+                    if el.is_a?(Array)
+                        new_arr.concat(el.my_flatten)
+                    else
+                        new_arr << el
+                    end
+                end
+            new_arr
+        end
+
+        def my_zip(*args)
+            new_arr = []
+
+            self.length.times do |i|
+                sub_arr = [self[i]]
+
+                args.each do |array|
+                    sub_arr << array[i]
+                end
+                new_arr <<  sub_arr
+            end
+            new_arr
+        end
+
+        def my_rotate(count = 1)
+            rotated = []
+            swap = count % self.length
+                rotated = self[swap..-1] + self[0...swap]
+            rotated
+        end
 
     end
 
 a = [1, 2, 3]
+[1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 p a.my_any? { |num| num > 1 } # => true
 p a.my_any? { |num| num == 4 } # => false
 p a.my_all? { |num| num > 1 } # => false
