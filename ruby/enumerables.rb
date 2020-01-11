0d8d09749bfe6111afeae57e73a32346f9963afa
michal <m.szymanski1995@gmail.com>
2020-01-11 16:20:50 +0100

    my_select

--- ruby/enumerables.rb
+++ ruby/enumerables.rb
@@ -9,18 +9,17 @@ class Array
     end
 
     def my_select(&block)
-        
-
+        new_arr = []
+        self.length.times do |i|
+            if block.call(self[i])
+                new_arr << i
+            end
+        end
+        new_arr
     end
 
 end
 
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-  end.my_each do |num|
-    puts num
-end
-
-puts ""
-
-p return_value
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
