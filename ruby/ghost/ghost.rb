82c68032bfdaea1950563f3b2f71c0ad8e19d9db
Michal Szymanski <m.szymanski1995@gmail.com>
2020-02-04 16:12:05 +0100

    d

--- ruby/ghost/ghost.rb
+++ ruby/ghost/ghost.rb
@@ -4,18 +4,28 @@ require_relative "player"
 
 class GhostGame
     ALPHABET = Set.new("a".."z")
-    
+    MAX_LOSS_COUNT = 5
 
     def initialize(*players)
         words = File.readlines("dictionary.txt").map(&:chomp)
         @dictionary = Set.new(words)
         @players = players
+        @losses = Hash.new { |losses,player| losses[player] = 0 }
     end
 
-
+    def run
+        play_round until game_over?
+        puts "#{winner} wins"
+    end
 
     #private
-    attr_reader :dictionary, :fragment
+    attr_reader :dictionary, :fragment, :losses, :players
+
+
+    def game_over?
+        remaining_players == 1
+    end
+
 
     def play_round
         @fragment = ""
@@ -24,6 +34,22 @@ class GhostGame
             take_turn
             next_player!
         end
+
+        update_standings
+    end
+
+    def winner
+        (player, _) = losses.find { |_, losses| losses < MAX_LOSS_COUNT }
+        player
+    end
+
+    def remaining_players
+        losses.count { |_, v| v < MAX_LOSS_COUNT }
+    end
+
+    def record(player)
+        count = losses[player]
+        "GHOST".slice(0,count)
     end
 
     def add_letter(letter)
@@ -63,8 +89,34 @@ class GhostGame
     def welcome
         system("clear")
         puts "Let's play a round of Ghost!"
-        #display_standings
-      end
+        display_standings
+    end
+
+    def display_standings
+        puts "Current standings:"
+    
+        players.each do |player|
+          puts "#{player}: #{record(player)}"
+        end
+    
+        sleep(2)
+    end
+
+    def update_standings
+        system("clear")
+        puts "#{previous_player} spelled #{fragment}."
+        puts "#{previous_player} gets a letter!"
+        sleep(1)
+    
+        if losses[previous_player] == MAX_LOSS_COUNT - 1
+          puts "#{previous_player} has been eliminated!"
+          sleep(1)
+        end
+        
+        losses[previous_player] += 1
+        
+        display_standings
+    end
 
     def take_turn
         system("clear")
@@ -90,6 +142,6 @@ if $PROGRAM_NAME == __FILE__
       Player.new("Agata"),
       )
   
-    game.play_round
+    game.run
 end
   
